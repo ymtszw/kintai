85d4979034901033e6f55ff138aba0038f30d3c9
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2016-02-09 21:57:39 +0900

    Delete cronfile

--- kintai.sh
+++ kintai.sh
@@ -3,5 +3,5 @@ KICKED="/tmp/kicked_`date "+%Y%m%d"`"
 [ -f $KICKED ] && exit 0 # Abort if already kicked today
 rm -f /tmp/kicked_*      # Delete old kicked files
 touch $KICKED            # Create kicked file for today
+echo "kicked: `date`"    # Log the time
 open https://teamspirit.ap0.visual.force.com/apex/AtkWorkComponent
-echo "kicked: `date`"
