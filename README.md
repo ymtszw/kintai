c9835bf685b62b832070f7b17e1edeb9c18352fb
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2016-02-09 22:34:48 +0900

    Install script

---
+++ README.md
@@ -0,0 +1,13 @@
+# kintai
+
+Pop TeamSpirit time stamper every morning (or afternoon)
+
+# Requirement
+
+* OSX
+
+# Install
+
+Copy below and paste in the Terminal, then push return key gently.
+
+`curl -sL https://raw.githubusercontent.com/YuMatsuzawa/kintai/master/install.sh | sh`
--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
-echo 'hello'
+KINTAI_DIR=$HOME/kintai
+[ -d $KINTAI_DIR ] || mkdir $KINTAI_DIR
+cd $KINTAI_DIR
+curl -sLO https://raw.githubusercontent.com/YuMatsuzawa/kintai/master/kintai.sh
+(crontab -l | sed '/kintai\.sh/d'; echo "* 8-23 * * MON-FRI sh $KINTAI_DIR/kintai.sh &> /tmp/kintai.log") | crontab -
+echo 'kintai installed'
